eeb7fcd56ce2464b377aa8645b6ab8aab8660a88
Karol Consoldane <karol.consoldane@gmail.com>
2018-06-09 15:57:45 -0500

    homework added

---
+++ Python_Twitter_Analysis/config.py
@@ -0,0 +1,6 @@
+
+
+consumer_key = "jCgu2f3Yg2vtyeyfpOIUuBFbz"
+consumer_secret = "FfuJudVBTCkq2oQq94qvUdbwflucimpgCgmQgNQnL8mAfz9MkA"
+access_token = "1004283557378973697-XyrqCTyBBsMV6UatjylEzWRjBgy3Zk"
+access_token_secret = "XsFO6IfjudnH2dDbrG1cKEsYPgfvHjJLL2e0TFB1PM32L"
